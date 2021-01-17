2711c8d02fcae409529138c21f34482f0f225192
maxingfang <maxingfang@meituan.com>
2021-01-17 22:16:40 +0800

    module_def.py

---
+++ module_def.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+"""
+
+
+
+"""
+import sys
+
+print('命令行参数如下:')
+for i in sys.argv:
+    print(i)
+
+print('\n\nPython 路径为：', sys.path, '\n')
+
+# python module_def.py 参数1 参数2
+
+
+
