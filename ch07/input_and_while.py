330f3f77b9fa3d0798865dd2c047d6308a77134e
maxingfang <maxingfang@meituan.com>
2023-05-30 15:15:15 +0800

    input_and_while-test

---
+++ ch07/input_and_while.py
@@ -0,0 +1,24 @@
+#!/use/bin/env python3
+
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nwhat is your first name?"
+
+name = input(prompt)
+print(f"\nHello, {name}!")
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+while len(unconfirmed_users) > 0:
+    current_user = unconfirmed_users.pop()
+    confirmed_users.append(current_user)
+    print(f"Verifying user: {current_user.title()}")
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
